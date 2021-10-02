f302e044a7184f276b0c2ddd444ad9ecb3ebe399
Anand Kumar <anandkmr740@gmail.com>
2021-10-02 11:52:16 +0530

    added entity

---
+++ src/main/java/com/springjpa/demo/entity/Student.java
@@ -0,0 +1,21 @@
+package com.springjpa.demo.entity;
+
+public class Student {
+	
+	private int id;
+	private int roll;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getRoll() {
+		return roll;
+	}
+	public void setRoll(int roll) {
+		this.roll = roll;
+	}
+	
+
+}
